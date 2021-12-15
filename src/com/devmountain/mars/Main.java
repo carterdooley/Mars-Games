6f57e5b476dfd83eb39fc265886cda12cd7cc495
Carter Dooley <carter.j.dooley43@gmail.com>
2021-12-15 11:18:47 -0600

    Finishing up

--- src/com/devmountain/mars/Main.java
+++ src/com/devmountain/mars/Main.java
@@ -38,6 +38,21 @@ public class Main {
         } else {
             System.out.println("I would reconsider what you are bringing");
         }
+        for (int x = 0; x == 0;) {
+            System.out.println("What type of animal will be your companion");
+            String companion = input.nextLine();
+            System.out.println("And what is your animal's name?");
+            String companionName = input.nextLine();
+            System.out.println("Joining you on your journey is " + companionName + ", which is a " + companion);
+            System.out.println("Is this correct? Please answer yes or no");
+            String response = input.nextLine();
+            if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
+                System.out.println("Amazing");
+                x++;
+            } else if (response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")) {
+                System.out.println("Please re-enter the information");
+            }
+        }
     }
 
 
